b75c9f44c87fa36618958a5138c65022cc44ff2e
spsnehalrp <spsnehalrp@gmail.com>
2017-01-06 11:31:49 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Downlaod</title>
+  </head>
+ <body>
+   <form action="upload.php" enctype="multipart/">
+      Book Name<input type="text" name="bname" id="bname"/>
+      Author Name<input type="text" name="aname" id="aname"/>
+      <input type="file" name="fname" id="fname"/>
+      <input type="submit" name="submit" value="submit"/>
+  </form>
+ </body>
+</html>
